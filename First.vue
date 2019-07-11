cccfc472df985554c67948e570b7a93ba2394709
Victor Vacarescu <victor.vacarescu@gmail.com>
2019-07-11 13:19:37 +0300

    uploaded manual

---
+++ First.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="demo">
+        <h1>{{msg}}</h1>
+        <button @click='interogareServer()'>Arata Utilizatori</button>
+        <ul>
+            <li v-for="utilizator in utilizatoriVue" :key="utilizator.Id">
+                {{utilizator.Email}}
+            </li>
+        </ul>
+    </div>
+</template>
+
+
+<script>
+export default {
+    name: 'First',
+    data() {
+        return {
+            utilizatoriVue: []
+        }
+    },
+    props: {
+        msg: String
+    },
+    methods: {
+        interogareServer(){
+            console.log('da')
+            this.$http.post('api/hei/getServerInfo',{IdUser:20}).then(
+                function(response){
+                    var body = response.body
+                    this.utilizatoriVue = body.Utilizatori
+                }
+            )
+        }
+    }
+}
+</script>
+
+<style>
+    .demo {
+        color: brown;
+        background-color: azure;
+    }
+</style>
+
---
+++ main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import VueResource from 'vue-resource'
+
+Vue.config.productionTip = false
+Vue.use(VueResource)
+
+Vue.http.options.root = 'http://www.vue.blog.menut.ro/';
+//Vue.http.headers.common['Token'] = ""
+//Vue.http.headers.common['IdUser'] = settings.get_id_user()
+//Vue.http.headers.common['AppCmd'] = ""
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
