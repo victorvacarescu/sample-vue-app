ae7f6405fcc1f9e746736d619b88f8fd8a292a86
victorvacarescu <victor.vacarescu@gmail.com>
2019-07-11 17:58:53 +0300

    final

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueResource from 'vue-resource'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
 Vue.config.productionTip = false
 Vue.use(VueResource)
