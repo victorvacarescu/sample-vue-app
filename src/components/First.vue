f7e664a9bb575ff2d433460da3573192d4f2f6b5
victorvacarescu <victor.vacarescu@gmail.com>
2019-07-12 17:34:35 +0300

    table element ajax

--- src/components/First.vue
+++ src/components/First.vue
@@ -1,36 +1,32 @@
 <template>
     <div class="demo">
         <h1>{{msg}}</h1>
-        <!-- <button @click='interogareServer()'>Arata Utilizatori</button> -->
-        <ul>
-            <li v-for="articol in articole" :key="articol.Id">
-                {{articol.Titlu}}
-                {{articol.DataInregistrare}}
-            </li>
-        </ul>
-        <div class="element">
-            <el-table
-                :data="tableData"
-                style="width: 100%">
-                <el-table-column                
-                    prop="date"
-                    label="Date"
-                    width="180">
-                </el-table-column>
-                <el-table-column
-                    prop="name"
-                    label="Name"
-                    width="180">
-                </el-table-column>
-                <el-table-column
-                    prop="address"
-                    label="Address">
-                </el-table-column>
-            </el-table>
-        </div>
+
+        <el-container>
+            <el-main>
+                <el-table
+                :data="articole"
+                style="width: 100%">                
+                    <el-table-column                                
+                        prop="Titlu"
+                        label="Titlu Articol"
+                        width="180">
+                    </el-table-column>
+                    <el-table-column                                
+                        prop="DataInregistrare"
+                        label="Data Articolului"
+                        width="180">
+                    </el-table-column>
+                    <el-table-column                                
+                        prop="Continut"
+                        label="Continut"
+                        width="180">
+                    </el-table-column>       
+                </el-table>
+            </el-main>            
+        </el-container>                          
     </div>
 </template>
-
 
 <script>
 export default {
@@ -50,16 +46,18 @@ export default {
     },
     methods: {
         interogareServer(){            
-            this.$http.post('api/hei/getServerInfo',{}).then(
+            this.$http
+            .post('api/hei/getServerInfo',{})
+            .then(
                 function(response){
-                    var body = response.body
-                    this.articole = body.Articole
+                    //var body = response.body
+                    this.articole = response.body.Articole
                 }
             )
         }
     },
     mounted(){
-        console.log('da')
+        //console.log('da')
         this.interogareServer();
     }
 }
