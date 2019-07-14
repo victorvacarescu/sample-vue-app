d571edbc65801b05b0f1d6a7dfb468bf1566695f
Victor Vacarescu <victor.vacarescu@gmail.com>
2019-07-15 01:26:38 +0300

    added cards

--- src/components/First.vue
+++ src/components/First.vue
@@ -1,30 +1,38 @@
 <template>
-    <div class="demo">
-        <h1>{{msg}}</h1>
-
-        <el-container>
-            <el-main>
-                <el-table
-                :data="articole"
-                style="width: 100%">                
-                    <el-table-column                                
-                        prop="Titlu"
-                        label="Titlu Articol"
-                        width="180">
-                    </el-table-column>
-                    <el-table-column                                
-                        prop="DataInregistrare"
-                        label="Data Articolului"
-                        width="180">
-                    </el-table-column>
-                    <el-table-column                                
-                        prop="Continut"
-                        label="Continut"
-                        width="180">
-                    </el-table-column>       
-                </el-table>
-            </el-main>            
-        </el-container>                          
+    <div class="demo">        
+        <b-jumbotron>
+            <h1>{{msg}}</h1>
+        </b-jumbotron>
+        <el-row>
+            <el-col 
+            :span="22" 
+            :offset="1">
+                <el-card>
+                    <el-row>
+                    </el-row>                                            
+                    <el-col 
+                    :span="6" 
+                    :offset="0" 
+                    v-for="articol in articole" :key="articol.Id">
+                        <el-card :body-style="{ padding: '0px' }" shadow="hover">
+                            <el-image                            
+                            :src='"http://www.blog.menut.ro/assets/img/" + articol.Imagine' 
+                            :fit="contain" 
+                            alt="" 
+                            class="image">
+                            </el-image>
+                            <div style="padding: 14px;">
+                                <title>{{articol.Titlu}}</title>
+                                <div class="bottom clearfix">
+                                    <h6>un text de test. un text de test</h6>                                
+                                    <el-button type="primary" icon="el-icon-view">Citeste</el-button>
+                                </div>
+                            </div>
+                        </el-card>
+                    </el-col>                                                
+                </el-card>  
+            </el-col>
+        </el-row>                              
     </div>
 </template>
 
@@ -33,12 +41,7 @@ export default {
     name: 'First',
     data() {
         return {
-            articole: [],
-            tableData: [{
-                date: '12.07.2019',
-                name: 'Victor',
-                address: 'aici'
-            }]
+            articole: [],                  
         }
     },
     props: {
@@ -56,16 +59,30 @@ export default {
             )
         }
     },
-    mounted(){
-        //console.log('da')
+    mounted(){        
         this.interogareServer();
+        //console.log(response);
     }
 }
 </script>
 
 <style>
-    .demo {
-        color: brown;        
+    .image {
+    width: 100%;
+    height:200px; 
+    display: block;
+    }
+    .bottom {
+    margin-top: 13px;
+    line-height: 12px;
+    }
+    .clearfix:before,
+    .clearfix:after {
+        display: table;
+        content: "";
+    }    
+    .clearfix:after {
+        clear: both
     }
 </style>
 
